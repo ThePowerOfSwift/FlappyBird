f94ea67ec26d0bfb8a05b6504ab804bb954cafda
snowmania0445 <sunoharatky@gmail.com>
2018-04-12 19:32:24 +0900

    課題途中

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -13,17 +13,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var scrollNode: SKNode!
     var wallNode: SKNode!
     var bird: SKSpriteNode!
+    var itemNode: SKNode!
     
     //衝突判定カテゴリー
     let birdCategory: UInt32 = 1 << 0       // 0...00001
     let groundCategory: UInt32 = 1 << 1     // 0...00010
     let wallCategory: UInt32 = 1 << 2       // 0...00100
     let scoreCategory: UInt32 = 1 << 3      // 0...01000
+    let itemCategory: UInt32 = 1 << 4
     
     //スコア
     var score = 0
+    var itemScore = 0
     var scoreLabelNode: SKLabelNode!
     var bestScoreLabelNode: SKLabelNode!
+    var itemScoreLabelNode: SKLabelNode!
     let userDefaults: UserDefaults = UserDefaults.standard
     
     override func didMove(to view: SKView) {
@@ -34,14 +38,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(scrollNode)
         
         //壁用のノード
-        wallNode = SKNode() //追加
+        wallNode = SKNode()
         scrollNode.addChild(wallNode)
+        
+        //アイテム用のノード
+        itemNode = SKNode()
+        scrollNode.addChild(itemNode)
         
         //各種スプライトを生成する処理をメソッドに分割
         setupGround()
         setupCloud()
         setupWall()
         setupBird()
+        setupItem()
         
         setupScoreLabel()
         
@@ -129,7 +138,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //画面外まで移動するアクションを作成
         let moveWall = SKAction.moveBy(x: -movingDistance, y: 0, duration:4.0)
         
-        //自信を取り除くアクションを作成
+        //自身を取り除くアクションを作成
         let removeWall = SKAction.removeFromParent()
         
         //2つのアニメーションを順に実行するアクションを作成
@@ -270,6 +279,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 userDefaults.set(bestScore, forKey: "BEST")
                 userDefaults.synchronize()
             }
+        } else if (contact.bodyA.categoryBitMask & itemCategory) == itemCategory || (contact.bodyB.categoryBitMask & itemCategory) == itemCategory {
+            print("itemScoreUp")
+            itemScore += 1
+            itemScoreLabelNode.text = "Score:\(itemScore)"
+            
         } else {
             // 壁か地面と衝突した
             print("GameOver")
@@ -320,8 +334,80 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let bestScore = userDefaults.integer(forKey: "BEST")
         bestScoreLabelNode.text = "Best Score:\(bestScore)"
         self.addChild(bestScoreLabelNode)
+        
+        itemScore = 0
+        itemScoreLabelNode = SKLabelNode()
+        itemScoreLabelNode.fontColor = UIColor.black
+        itemScoreLabelNode.position = CGPoint(x: 10, y: self.frame.size.height - 120)
+        itemScoreLabelNode.zPosition = 100 // 一番手前に表示する
+        itemScoreLabelNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+        itemScoreLabelNode.text = "Item Score:\(itemScore)"
+        self.addChild(itemScoreLabelNode)
     }
     
+    //ここから課題用のメイン処理
+    func setupItem() {
+        //アイテムの画像を読み込む
+        let itemTexture = SKTexture(imageNamed: "leaf")
+        itemTexture.filteringMode = .linear
+        
+        //移動する距離を計算
+        let movingDistance = CGFloat(self.frame.size.width + itemTexture.size().width)
+        
+        //画面外まで移動するアクションを作成
+        let moveItem = SKAction.moveBy(x: -movingDistance, y: 0, duration:4.0)
+        
+        //自身を取り除くアクションを作成
+        let removeItem = SKAction.removeFromParent()
+        
+        //2つのアニメーションを順に実行するアクションを作成
+        let itemAnimation = SKAction.sequence([moveItem, removeItem])
+        
+        //アイテムを生成するアクションを作成
+        let createItemAnimation = SKAction.run({
+            //アイテム関連のノードを乗せるノードを作成
+            let item = SKNode()
+            item.position = CGPoint(x: self.frame.size.width + itemTexture.size().width / 2, y: 0.0)
+            item.zPosition = -70.0
+            
+            //画面のY軸中央値
+            let center_y = self.frame.size.height / 2
+            //アイテムのY座標を上下ランダムにさせる時の最大値
+            let random_y_range = self.frame.size.height / 4
+            //アイテムのY軸の下限
+            let item_lowest_y = UInt32( center_y - itemTexture.size().height / 2 - random_y_range / 2)
+            //1〜random_y_rangeまでのランダムな整数を生成
+            let random_y = arc4random_uniform( UInt32(random_y_range) )
+            //Y軸の下限にランダムな値を足して、下の壁のY座標を決定
+            let item_y = CGFloat(item_lowest_y + random_y)
+            
+            //アイテムを作成
+            let item_leaf = SKSpriteNode(texture: itemTexture)
+            item_leaf.position = CGPoint(x: 0.0, y: item_y)
+            item.addChild(item_leaf)
+            
+            //スプライトに物理演算を設定する
+            item_leaf.physicsBody = SKPhysicsBody(circleOfRadius: item_leaf.size.height / 2.0)
+            item_leaf.physicsBody?.categoryBitMask = self.itemCategory
+            
+            //衝突の際に動かないように設定する
+            item_leaf.physicsBody?.isDynamic = false
+            
+            item.run(itemAnimation)
+            
+            self.itemNode.addChild(item)
+            
+    })
+    
+        //次のアイテム作成までの待ち時間のアクションを作成
+        let waitAnimation = SKAction.wait(forDuration: 5)
+        
+        //アイテムを作成→待ち時間→アイテムを作成を無限に繰り返すアクション
+        let repeatForeverAnimation = SKAction.repeatForever(SKAction.sequence([createItemAnimation, waitAnimation]))
+        
+        itemNode.run(repeatForeverAnimation)
+
+    }
 }
 
 
